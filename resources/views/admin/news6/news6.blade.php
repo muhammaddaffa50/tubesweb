096f63debd3216d952ae3a9fbc48339944742e12
muhammaddaffa50 <wantedmaster50@webmail.umm.ac.id>
2021-07-02 06:14:10 +0700

    panderman

--- resources/views/admin/news6/news6.blade.php
+++ resources/views/admin/news6/news6.blade.php
@@ -6,13 +6,13 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0">kawi</h1>
+        <h1 class="m-0">panderman</h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="/">Home</a></li>
           <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
-          <li class="breadcrumb-item active">kawi</li>
+          <li class="breadcrumb-item active">panderman</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
@@ -26,7 +26,7 @@
     <div class="card">
       <div class="card-header">
         <div class="d-flex justify-content-between">
-          <h3 class="card-title">inputan artikel kawi</h3>
+          <h3 class="card-title">inputan artikel panderman</h3>
           <a href="{{route('news6.create')}}" class="btn btn-sm btn-success"><i class="fas fa-plus"></i> Create</a>
         </div>
       </div>
